935517af469664b0ca472b46b85e9131f2230896
juyoung <anwjsrlrhwkd@naver.com>
2021-12-17 13:07:56 +0900

    ,

--- voicing_project/src/refactor.py
+++ voicing_project/src/refactor.py
@@ -1,30 +1,35 @@
 # TDD로 다시 짜기
 
-import unittest, pytest
+
+class ChromaticScale:
+    def __init__(self) -> None:
+        '''음역대 정보'''
+        self._chromatic_scales = ('C', 'Db', 'D', 'Eb',
+                         'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B')
+        self._octaves = ('1', '2', '3', '4')
+        self._long_chromatic = self.get_long_chromatic()
+        print(self._long_chromatic)
+    def get_long_chromatic(self):  
+        ''' C1~B4 생성  '''
+        return tuple(i + j for j in self._octaves
+                        for i in self._chromatic_scales)
 
 class SongKey:
+    
     def __get__(self, instance, owner):
-        # try:
+        try:
             print(f'{instance}의 키는 {instance.__dict__[self.key]}입니다')
-        # except AttributeError:
-        #     print( '키가 생성되지 않았습니다')
+        except AttributeError:
+            print( '키가 생성되지 않았습니다')
 
     def __set__(self, instance, value):
         pass
 
 class Voicer:
-    key = SongKey()
 
     def __init__(self) -> None:
         pass
 
     
+a = ChromaticScale()
 
-
-class TestVoicer(unittest.TestCase):
-    def test_song_key(self):
-        with self.assertRaises(AttributeError):
-            tester = Voicer()   
-            tester.key
-
-unittest.main()
---
+++ voicing_project/test/test.py
@@ -0,0 +1,8 @@
+import pytest
+from project.voicing_project.src.refactor import Voicer, ChromaticScale, SongKey
+
+
+@pytest.fixture
+def TestChromaticScale():
+    scale = ChromaticScale
+    assert scale._long_chromatic == ('C1', 'Db1', 'D1', 'Eb1', 'E1', 'F1', 'Gb1', 'G1', 'Ab1', 'A1', 'Bb1', 'B1', 'C2', 'Db2', 'D2', 'Eb2', 'E2', 'F2', 'Gb2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2', 'C3', 'Db3', 'D3', 'Eb3', 'E3', 'F3', 'Gb3', 'G3', 'Ab3', 'A3', 'Bb3', 'B3', 'C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4')
