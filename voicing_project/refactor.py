036806fd2cf38c07af9b29a33b44a6f4bac55bb5
juyoung <anwjsrlrhwkd@naver.com>
2021-12-23 11:39:36 +0900

    key값 검사, 마이너/메이져속성설정 분리

--- voicing_project/refactor.py
+++ voicing_project/refactor.py
@@ -1,6 +1,7 @@
 # TDD로 다시 짜기
 
 
+
 class ChromaticScale:
     def __init__(self) -> None:
         '''스케일 정보'''
@@ -20,22 +21,36 @@ class ChromaticScale:
         '''
         속성 self._chromatic_scale을 입력된 key가 첫 인덱스가 되도록 정렬
         '''
+        
         prev_scale = self._chromatic_scale
         idx = prev_scale.index(key[0])
         self._chromatic_scale = prev_scale[idx:]+prev_scale[:idx]
     
+def assert_key(key: str):
+    assert type(key) == str, f'{key}는 잘못된 key 입니다'
+    assert len(key) < 3, f'{key}는 잘못된 key 입니다'
 
 class Chord(ChromaticScale):
-    def __init__(self, key: str) -> None:
+    def __init__(self, key: str = 'C') -> None:
         ''' 코드 정보 '''
         super().__init__()
+        assert_key(key)
         self._key = key
         self._sorted_dia_chord_form = ['', 'M7'], ['m', 'm7'], [
         'm', 'm7'], ['', 'M7'], ['', '7'], ['m', 'm7'], ['mb5', 'm7b5']
         self._chord_tone = {'': [0, 4, 7], 'm': [0, 3, 7], 'M7': [0, 4, 7, 11], 'm7': [
         0, 3, 7, 10], '7': [0, 4, 7, 10], 'mb5': [0, 3, 6], 'm7b5': [0, 3, 6, 10]}
+        
+        self._diatonic_note_idx = []
+        self.set_minor_attr() if 'm' in self._key else self.set_major_attr()
         self.dictonic = self._get_diatonic()
 
+    def set_minor_attr(self) -> None:
+        self._diatonic_note_idx = self._minor_dia_scale_idx
+        self._resort_minor_dia_chord_form()
+
+    def set_major_attr(self) -> None:
+        self._diatonic_note_idx = self._major_dia_scale_idx
 
     def _get_diatonic(self) -> list:
         ''' 
@@ -44,15 +59,10 @@ class Chord(ChromaticScale):
         ['C','M7],['D','m7'].... 으로 리스트 반환
         '''
         diatonic_chords = []
-        if 'm' in self._key:
-            self._resort_minor_dia_chord_form()
-            diatonic_note_idx = self._minor_dia_scale_idx
-        else:
-            diatonic_note_idx = self._major_dia_scale_idx
-
+            
         self.sort_chromatic_by(self._key) # self._chromatic_scale 정렬
 
-        for idx, note in enumerate(diatonic_note_idx):
+        for idx, note in enumerate(self._diatonic_note_idx):
             diatonic_chords.append(
                 [self._chromatic_scale[note], 
                 self._sorted_dia_chord_form[idx][1]])
@@ -76,5 +86,5 @@ class Voicer:
 
     
 
-C = Chord('D')
+C = Chord()
 print(C.dictonic)
